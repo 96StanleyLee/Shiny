cd8bef92ad8f58792f1e5838d93ef964bd0a73dc
96StanleyLee <96stanleylee@gmail.com>
2020-08-17 18:13:35 -0400

    fixed up forms

--- src/Containers/Register.js
+++ src/Containers/Register.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
+
 
 const Register = () => {
+
+  let [firstName, setFirstName] = useState('')
+  let [lastName, setLastName] = useState('')
+  let [email, setEmail] = useState('')
+  let [password, setPassword] = useState('')
+  let [passwordRepeat, setPasswordRepeat] = useState('')
+
+  
+
+
   return (
     <div className="section register">
       <div className="container">
         <h1>Register</h1>
 
         <p>Please fill in this form to create an account.</p>
+
+
+        <label for="firstname">
+          <b>First Name</b>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter First Name"
+          name="firstName"
+          id="firstname"
+          value={firstName}
+          onChange={(e)=>setFirstName(e.target.value)}
+          required
+        />
+        
+        <br></br>
+
+        <label for="lastname">
+          <b>Last Name</b>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Last Name"
+          name="lastName"
+          id="lastname"
+          value={lastName}
+          onChange={(e)=>setLastName(e.target.value)}
+          required
+        />
+        <br></br>
+
 
         <label for="email">
           <b>Email</b>
@@ -16,6 +59,8 @@ const Register = () => {
           placeholder="Enter Email"
           name="email"
           id="email"
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
           required
         />
         <br></br>
@@ -26,8 +71,10 @@ const Register = () => {
         <input
           type="password"
           placeholder="Enter Password"
-          name="psw"
+          name="password"
           id="psw"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
           required
         />
         <br></br>
@@ -38,8 +85,10 @@ const Register = () => {
         <input
           type="password"
           placeholder="Repeat Password"
-          name="psw-repeat"
+          name="passwordRepeat"
           id="psw-repeat"
+          value={passwordRepeat}
+          onChange={(e)=>setPasswordRepeat(e.target.value)}
           required
         />
         <br></br>
