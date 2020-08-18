625ee8f410163147dfea5915c6bd36e2d9227e9f
Rock Vincent Guitard <rvguitard@gmail.com>
2020-08-17 20:08:56 -0400

    test

--- src/Containers/Register.js
+++ src/Containers/Register.js
@@ -5,42 +5,46 @@ const Register = () => {
     <div className="section register">
       <div className="container">
         <h1>Register</h1>
-
         <p>Please fill in this form to create an account.</p>
-
         <label for="email">
           <b>Email</b>
-        </label>
+        </label>{" "}
+        <br />
         <input
+          className="mb2"
           type="text"
           placeholder="Enter Email"
           name="email"
           id="email"
           required
-        />
-
+        />{" "}
+        <br />
         <label for="psw">
           <b>Password</b>
         </label>
+        <br />
         <input
+          className="mb2"
           type="password"
           placeholder="Enter Password"
           name="psw"
           id="psw"
           required
-        />
-
+        />{" "}
+        <br />
         <label for="psw-repeat">
           <b>Repeat Password</b>
         </label>
+        <br />
         <input
+          className="mb2"
           type="password"
           placeholder="Repeat Password"
           name="psw-repeat"
           id="psw-repeat"
           required
-        />
-
+        />{" "}
+        <br />
         <p>
           By creating an account you agree to our{" "}
           <a href="#">Terms &amp; Privacy</a>.
