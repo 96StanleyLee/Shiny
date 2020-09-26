8c88e920138d6b6d20cf8fafaa955d3853125f99
Rock Vincent Guitard <rvguitard@gmail.com>
2020-09-26 14:49:25 -0400

    html

--- src/Containers/Footer.js
+++ src/Containers/Footer.js
@@ -2,8 +2,18 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="container">hi this is a footer</div>
+    <div className="footer grid-container">
+        <div className="Side">
+          <h2>Footer</h2></div>
+          <div className="Main">
+            <span className="footer-link">About</span>
+            <span className="footer-link">FAQ</span>
+            <span className="footer-link">Contact</span>
+            <span className="footer-link">Terms of Service</span>
+            <span className="footer-link">Privacy Policy</span>
+            <span className="footer-link">Twitter</span>
+            <span className="copyright">A project by Stanley &amp; Rox</span>
+          </div>
     </div>
   );
 };
--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -50,6 +50,82 @@ const HomePage = ({ homecards, user }) => {
           </div>
         </div>
       </div>
+      <div className="grid-container">
+        <div className="Side">
+          <h1>Ideas being polished</h1>
+          <span className="category-btn">Social</span>
+          <span className="category-btn">Community</span>
+          <span className="category-btn">Tool</span>
+          <span className="category-btn">Concept</span>
+        </div>
+        <div className="Main-p0 lg-card-grid">
+          {homecards.map((cards) => {
+            return <ShinyCard key={cards.id} data={cards} />;
+          })}
+          <div className="grid-item view-more">
+            <span className="big-btn">
+              View More
+              <div className="big-btn-bg"></div>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="grid-container">
+        <div className="Side">
+          <h1>How it works</h1>
+        </div>
+        <div className="Main-p0 ">
+          <div className="">
+          <h2>Publish and Create Interest</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit orci fermentum orci, enim auctor turpis integer. Natoque sed vivamus tellus ac praesent. In sed elementum platea viverra non. Orci tincidunt maecenas pellentesque aliquam interdum. Nisi, quis in ac ipsum sem quisque. Massa amet eget leo sit. Massa pretium at et nulla nulla. Senectus dictumst id et vitae leo justo.</p>
+          <h2>Find Partners</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit orci fermentum orci, enim auctor turpis integer. Natoque sed vivamus tellus ac praesent. In sed elementum platea viverra non. Orci tincidunt maecenas pellentesque aliquam interdum. Nisi, quis in ac ipsum sem quisque. Massa amet eget leo sit. Massa pretium at et nulla nulla. Senectus dictumst id et vitae leo justo.</p>
+          </div>
+          <div className="">
+          <h2>Learn and Collaborate</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit orci fermentum orci, enim auctor turpis integer. Natoque sed vivamus tellus ac praesent. In sed elementum platea viverra non. Orci tincidunt maecenas pellentesque aliquam interdum. Nisi, quis in ac ipsum sem quisque. Massa amet eget leo sit. Massa pretium at et nulla nulla. Senectus dictumst id et vitae leo justo.</p>
+          <h2>Have fun, make friends</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit orci fermentum orci, enim auctor turpis integer. Natoque sed vivamus tellus ac praesent. In sed elementum platea viverra non. Orci tincidunt maecenas pellentesque aliquam interdum. Nisi, quis in ac ipsum sem quisque. Massa amet eget leo sit. Massa pretium at et nulla nulla. Senectus dictumst id et vitae leo justo.</p>
+          </div>
+        </div>
+      </div>
+      <div className="grid-container">
+        <div className="Side">
+          <h1>The Shiny System</h1>
+        </div>
+        <div className="Main-p0 ">
+          <div className="">
+          <h2>Gem Ratings</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit orci fermentum orci, enim auctor turpis integer. Natoque sed vivamus tellus ac praesent. In sed elementum platea viverra non. Orci tincidunt maecenas pellentesque aliquam interdum. Nisi, quis in ac ipsum sem quisque. Massa amet eget leo sit. Massa pretium at et nulla nulla. Senectus dictumst id et vitae leo justo.</p>
+        </div>
+        <div className="">
+        <div className="">
+          <h3>A Shiny Idea</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi augue lobortis quis est sed dolor. Imperdiet parturient rhoncus tincidunt id risus augue. Sed amet, nulla dolor, elit lacus id. Nunc cras suspendisse egestas varius vel.</p>
+        </div>
+        <div className="">
+          <h3>Purple</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi augue lobortis quis est sed dolor. Imperdiet parturient rhoncus tincidunt id risus augue. Sed amet, nulla dolor, elit lacus id. Nunc cras suspendisse egestas varius vel.</p>
+        </div>
+        <div className="">
+          <h3>Crimson</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi augue lobortis quis est sed dolor. Imperdiet parturient rhoncus tincidunt id risus augue. Sed amet, nulla dolor, elit lacus id. Nunc cras suspendisse egestas varius vel.</p>
+        </div>
+        <div className="">
+          <h3>Magenta</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi augue lobortis quis est sed dolor. Imperdiet parturient rhoncus tincidunt id risus augue. Sed amet, nulla dolor, elit lacus id. Nunc cras suspendisse egestas varius vel.</p>
+        </div>
+        <div className="">
+          <h3>Beginning</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi augue lobortis quis est sed dolor. Imperdiet parturient rhoncus tincidunt id risus augue. Sed amet, nulla dolor, elit lacus id. Nunc cras suspendisse egestas varius vel.</p>
+        </div>
+        <div className="">
+          <h3>Starter</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi augue lobortis quis est sed dolor. Imperdiet parturient rhoncus tincidunt id risus augue. Sed amet, nulla dolor, elit lacus id. Nunc cras suspendisse egestas varius vel.</p>
+        </div>
+        </div>
+      </div>
+      </div>
       <div className="homepage-bg">
         <img src={LinesBG} alt="Lines Background" />
       </div>
